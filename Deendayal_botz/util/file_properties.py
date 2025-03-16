e8b2e9a6a1d0702041958f092c30e1698e24fa09
Himanshu-4444 <154578231+Himanshu-4444@users.noreply.github.com>
2025-03-16 17:31:42 +0530

    Update file_properties.py

--- Deendayal_botz/util/file_properties.py
+++ Deendayal_botz/util/file_properties.py
@@ -1,4 +1,4 @@
-#Thanks @dreamcinezone for helping in this journey 
+#Thanks @VisualMovies for helping in this journey 
 from pyrogram import Client
 from typing import Any, Optional
 from pyrogram.types import Message
