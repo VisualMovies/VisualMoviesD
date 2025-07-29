60ee3669cc00f4803b94812e75fd78aacf822f85
VisualMovies <VisualMovies3@gmail.com>
2025-07-29 20:11:58 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -271,7 +271,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/Deendayal_dhakad">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href=https://t.me/Deendayal_dhakad>[Deendayal_Dhakad]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/VisualMovies1">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href=https://t.me/VisualMovies1>[Visual Movies™]</a></b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>🏷 Title</b>: <a href={url}>{title}</a>
